a78d3e48fccaa4974f14583b84444436e15af597
Turt1e26 <bodiewolf26@gmail.com>
2023-03-02 09:42:53 -0800

    finished

--- Puzzle/Puzzle/DragableImage.swift
+++ Puzzle/Puzzle/DragableImage.swift
@@ -11,11 +11,27 @@ struct DragableImage: View {
     private let imageSize: CGFloat = 100
     @State var offset = CGSize.zero
     @State var imageName = "1"
+    var endLocation: CGPoint
     var dragGesture: some Gesture{
         DragGesture()
             .onChanged{ value in
-                offset = CGSize(width: value.startLocation.x + value.translation.width - imageSize/2, height: value.startLocation.y + value.translation.height - imageSize/2)
+                var x = value.startLocation.x + value.translation.width - imageSize/2
+                var y = value.startLocation.y + value.translation.height - imageSize/2
+                
+                
+                if abs(x - endLocation.x) < 20 && abs(y - endLocation.y) < 20{
+                    x = endLocation.x
+                    y = endLocation.y
+                }
+                
+                offset = CGSize(width:x, height:y)
+                    
             }
+    }
+    init (imageName: String, endLocation: CGPoint){
+        self.offset = CGSize(width: Int.random(in: -200...200), height: Int.random(in: -200...200))
+        self.imageName = imageName
+        self.endLocation = endLocation
     }
     var body: some View {
         VStack {
@@ -26,13 +42,15 @@ struct DragableImage: View {
                 .offset(offset)
                 .gesture(dragGesture)
             
-            
+           
         }
+        
     }
+    
 }
 
-struct DragableImage_Previews: PreviewProvider {
-    static var previews: some View {
-        DragableImage()
-    }
-}
+//struct DragableImage_Previews: PreviewProvider {
+//    static var previews: some View {
+//        DragableImage()
+//    }
+//}
